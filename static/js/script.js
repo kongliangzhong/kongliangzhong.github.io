69fe42f5d16731f99ef9e504fd3b5144da27b387
Kongliang Zhong <zhongkl@gmail.com>
2015-09-07 13:40:26 +0800

    update

--- static/js/script.js
+++ static/js/script.js
@@ -40,9 +40,9 @@ function backToTop() {
 
 function search(){
     (function(w,d,t,u,n,s,e){w['SwiftypeObject']=n;w[n]=w[n]||function(){
-        (w[n].q=w[n].q||[]).push(arguments);};s=d.createElement(t);
-        e=d.getElementsByTagName(t)[0];s.async=1;s.src=u;e.parentNode.appendChild(s);
-    })(window,document,'script','//s.swiftypecdn.com/install/v2/st.js','_st');
+         (w[n].q=w[n].q||[]).push(arguments);};s=d.createElement(t);
+         e=d.getElementsByTagName(t)[0];s.async=1;s.src=u;e.parentNode.insertBefore(s,e);
+         })(window,document,'script','//s.swiftypecdn.com/install/v2/st.js','_st');
 
-    _st('install','y9ybYELW6wqvLgv7YLiQ','2.0.0');
+_st('install','i_q6LQiQDtWoMqbjkVMi','2.0.0');
 }
